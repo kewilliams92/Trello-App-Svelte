59b688fcde21efbd5446d094984f87acbf886e08
kw92 <kewilliams2017@hotmail.com>
2023-04-09 05:57:15 -0400

    editable values

---
+++ src/components/task-manager/Editable.svelte
@@ -0,0 +1,20 @@
+<script>
+    export let defaultValue;
+    let isEditing = false;
+    let value = defaultValue;
+</script>
+
+{#if isEditing}
+	<div class="flex-it">
+		<!-- <textarea bind:value={value}/> -->
+        <textarea bind:value />
+        <button 
+        on:click={() => isEditing = false}
+        class="flex underline"> Close </button>
+        
+	</div>
+{:else}
+<button on:click={() => isEditing = true}>
+	<slot />
+</button>
+{/if}
--- src/routes/task-manager/+page.svelte
+++ src/routes/task-manager/+page.svelte
@@ -1,4 +1,29 @@
+<script>
+import TaskList from "../../components/task-manager/TaskList.svelte";
 
-<div class='text-white'>
-i am a task manager page
+const taskList = [
+    {id: "l-1", text: "List 1", items: [{id: "t-1", text: "Task 1"},{id: "t-2", text: "Task 2"},{id: "t-3", text: "Task 3"}]},
+    {id: "l-2", text: "List 2", items: [{id: "t-4", text: "Task 4"},{id: "t-5", text: "Task 5"},{id: "t-6", text: "Task 6"}]},
+    {id: "l-3", text: "List 3", items: [{id: "t-7", text: "Task 7"},{id: "t-8", text: "Task 8"},{id: "t-9", text: "Task 9"}]},
+  ];
+</script>
+
+
+
+<div class="p-10 h-full">
+	<div class="text-white text-2xl mb-6">Some List</div>
+	<button class="text-xl mb-3 text-white font-bold cursor-pointer hover:underline flex items-start">
+		+ Add List
+	</button>
+	<div class="flex-it h-full">
+		<div class="flex-it flex-row rounded-xl h-full">
+            {#each taskList as list (list.id)}
+                <TaskList 
+                listName={list.text} 
+                tasks={list.items}
+                />
+            {/each}
+
+		</div>
+	</div>
 </div>
